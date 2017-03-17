386863c54ff98120e9d84029a8aeb2b27727c95c
kanchankhatri <kanchan@kanchans-MacBook-Pro.local>
2017-03-17 15:34:30 +0530

    add example_api usage file and Readme

---
+++ Readme.md
@@ -0,0 +1,29 @@
+[Sort Trip Based On Source and Destination]
+==============================================
+Description 
+----------------------------------------------
+Given: Tickets of various Transports with source, dest and additonal info.
+A Complete Trip is to be returned by sorting the tickets in order of their source and dest.
+### Dependencies
+- PHP 5.6
+
+### How to use
+* example_api.php is example usage of the classes.
+* It creats Ticket Objects and Pass these array of Objects to Trip sort function which will return the sorted array of Ticket Objects. 
+
+Basic Design 
+----------------------------------------------        
+    Base
+    ├── TicketFactory - will create Ticket Objects based on transport. By default, GenericTicket is the class for Ticket with no transport class.
+    └── Ticket
+        └── TrainTicket            
+        └── FlightTicket            
+        └── GenericTicket            
+    └── Trip - Trip Planner
+
+    Helper Functions and View files are used by classes to process and display the required info.
+
+To Add
+----------------------------------------------
+Test Cases are not yet added, This would take little time. Dependencies will be updated accordingly.
+
---
+++ example_api.php
@@ -0,0 +1,30 @@
+<?php
+/**Author: Kanchan Khatri
+ * This is the example usage of the API. Input - Associative Array of Tickets
+ * Steps to perform: 
+ * 1. Create an Array of TicketFactory Objects. Each TicketFactory Object is the Ticket formed from the assoc array of Ticket.
+ * 2. Pass Array of TicketFactory Objects to Trip.
+ * 3. call Trip fn - arrange_tickets which will output the response.
+ */
+
+require_once 'application/autoload.php';
+// sample tickets
+$tickets[2]=array('source'=>'madrid','destination'=>'barcelona','transport'=>'train','seat'=>'45B','train_no'=>'78A');
+$tickets[1]=array('source'=>'barcelona','destination'=>'gerona_airport','transport'=>'bus');
+$tickets[3]=array('source'=>'gerona_airport','destination'=>'stockholm','transport'=>'flight','seat'=>'3A','flight_no'=>'SK455','gate'=>'45B','baggage'=>'ticket counter 344');
+$tickets[0]=array('source'=>'Stockholm','destination'=>'new_york_jfk','transport'=>'flight','seat'=>'7B','flight_no'=>'SK22','gate'=>'22','baggage'=>'automatically transferred from your last leg');
+
+
+// create Ticket Objects and assign in an array,
+foreach ($tickets as $key => $ticket) {
+
+	$ticket_arr[] =  TicketFactory::create($ticket);
+}
+
+
+$t = new Trip($ticket_arr);
+$response = $t->arrange_tickets();
+if($response['status']=='success')
+	echo $response['msg'];
+else	
+	echo 'Error In Sorting Tickets - '.$response['msg'];
